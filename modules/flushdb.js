09568f3fa281005ec4dd2d422f6d26ca86b0e489
Prateek Sanodiya <prateeksanodiya937@gmail.com>
2024-06-08 12:33:08 +0530

    async import

--- modules/flushdb.js
+++ modules/flushdb.js
@@ -1,5 +1,5 @@
 const {run} = require('./run');
-
+const async = require('async');
 module.exports.flushall = (nodes, next) => {
     async.forEach(nodes, (node, nodeName, next) => {
         const command = `redis-cli -h ${node} -p ${port} flushall`;
