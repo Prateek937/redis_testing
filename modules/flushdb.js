f08e3115292e4c0be5075ae294bfe7646d0a6eae
Prateek Sanodiya <prateeksanodiya937@gmail.com>
2024-06-08 13:23:09 +0530

    dsds

--- modules/flushdb.js
+++ modules/flushdb.js
@@ -1,7 +1,7 @@
 const {run} = require('./run');
 const async = require('async');
 module.exports.flushall = (nodes, next) => {
-    async.forEach(nodes, (node, nodeName, next) => {
+    async.eachOf(nodes, (node, nodeName, next) => {
         const command = `redis-cli -h ${node.private_ip} -p ${node.port} flushall`;
         run(command, next);
     }, next);
